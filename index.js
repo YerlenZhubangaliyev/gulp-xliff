b1af89975b9f9e922e81486fbed31a83db536189
Yerlen Zhubangaliyev <yz@yz.kz>
2015-01-29 17:55:25 +0600

    simple format

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var
 through       = require('through2'),
 fs            = require('fs'),
 xml2js        = require('xml2js'),
+_             = require('lodash'),
 jsonpath      = require('JSONPath'),
 gutil         = require('gulp-util'),
 ext           = gutil.replaceExtension,
@@ -19,28 +20,37 @@ pluginName    = "gulp-xliff2json";
 
 
 function parseXliff (xliff, cb) {
-    "use strict";
-
     xmlParser.parseString(xliff, function (err, json) {
         cb(json);
     });
 };
 
-function handleOutput (output, file, cb) {
-    file.path = ext(file.path, '.json');
-    file.contents = new Buffer(output);
+function handleOutput (inputJson, file, cb) {
+    var stringifiedJson = findAllTransUnits(inputJson);
+
+    file.path     = ext(file.path, '.json');
+    file.contents = new Buffer(stringifiedJson);
     cb(null, file);
 }
 
 function findAllTransUnits (jsonObject) {
-    jsonpath.eval(jsonObject, transUnitPath);
+    var
+        result         = {},
+        identifiers    = jsonpath.eval(jsonObject, "$..trans-unit..$.id"),
+        sources        = jsonpath.eval(jsonObject, "$..trans-unit..source[0]"),
+        sourceLanguage = jsonpath.eval(jsonObject, "$..source-language");
+
+    _.each(identifiers, function (identifierValue, index) {
+        result[identifierValue] = sources[index];
+    });
+
+    return JSON.stringify(result, null, 4);
 }
 
 module.exports = function () {
     function xliff2Json (file, encoding, cb) {
         parseXliff(String(file.contents), function (xliffJson) {
-            var json = findAllTransUnits(xliffJson);
-            handleOutput(JSON.stringify(json, null, 4), file, cb);
+            handleOutput(xliffJson, file, cb);
         });
 
     }
